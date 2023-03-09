4f286e6d387bdee1dba7d8f33943c7c972d1d783
Arum Bettina <ugwuanyibettina@gmail.com>
2023-03-09 12:05:47 +0100

    forms,CRUD,user accounts

--- Blog/urls.py
+++ Blog/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('',include('post.urls')),
+    path('accouts/',include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('', include ('post.urls')),
 ]
--- post/views.py
+++ post/views.py
@@ -1,9 +1,27 @@
 from django.views.generic import ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import post
 # Create your views here.
 class HomePageView(ListView):
     model = post
     template_name = 'home.html'
+
 class BlogDetailView(DetailView):
     model =post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model =post
+    template_name = 'post_new.html'
+    fields =['title','author','body']
+
+class BlogUpdateView(UpdateView):
+    model =post
+    template_name = 'post_edit.html'
+    fields =['title','body']
+
+class BlogDeleteView(DeleteView):
+    model =post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
